42c03d9ad3fc1b53f0e569ebc82ad1f8d1415288
Tanziruz <Tanziruz25@gmail.com>
2026-02-24 19:52:11 +0530

    Added IndoCard.tsx

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,13 +1,14 @@
-import { Recycle } from "lucide-react";
-import FeatureCard from "../Cards/FeatureCard";
+import { Mail } from "lucide-react";
+import InfoCard from "../Cards/InfoCard";
+
 
 export default function Home(){
     return (
         <>
         <section className="h-dvh max-w-screen w-full overflow-x-hidden flex flex-col justify-center items-center">
             <h1 className="text-center">Premium Wear For Modern Living</h1>
-            <p className="t18 text-center"  >Discover our new range of soft clothes made for your daily look and your best days with the finest fabrics.</p>
-            <FeatureCard title="Sustainable Materials" icon={Recycle} info="Our commitment to sustainability means we use eco-friendly materials and ethical production practices to create stylish, high-quality clothing that you can feel good about wearing."/>
+          <p className="t18 text-center"  >Discover our new range of soft clothes made for your daily look and your best days with the finest fabrics.</p>
+            <InfoCard title="Email Address" info="contact@blackinkkk.com" icon={Mail}/>
         </section>
         </>
     );
