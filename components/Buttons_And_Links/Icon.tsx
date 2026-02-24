14df922b4e0d737de5fb0094c5ae2973a5446552
Tanziruz <Tanziruz25@gmail.com>
2026-02-24 15:07:01 +0530

    Changed the location of Buttons and Links components.

---
+++ components/Buttons_And_Links/Icon.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import type { LucideIcon } from "lucide-react";
+
+interface SearchProps {
+    variant?: "variant1" | "variant2";
+    icon: LucideIcon;
+}
+
+export default function Icon({ variant = "variant1", icon: Icon }: SearchProps) {
+    return (
+        <>
+            <div className={clsx(
+                "w-9.25 h-9 hover:cursor-pointer hover:scale-110 flex justify-center transition duration-200 items-center flex-wrap rounded-full",
+                {
+                    "bg-white-15 hover:bg-white-30": variant === "variant1",
+                    "bg-white hover:bg-white-80 ": variant === "variant2",
+                }
+            )}>
+                <div className="w-auto h-auto">
+                    <Icon color={variant === "variant1" ? "#ffffff" : "#000000"} />
+                </div>
+            </div>
+        </>
+    );
+}
--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,19 +1,10 @@
-import { Search, Tag } from "lucide-react";
-import Button from "../Button";
-import Nav_Search from "../Nav_Search";
-import Nav_Link from "../Nav_Link";
-import Tag_Link from "../Tag_Link";
-import Footer_Link from "./Footer_Link";
-
 export default function Home(){
     return (
         <>
         <section className="h-dvh w-screen flex flex-col justify-center items-center">
             <h1 className="text-center">Premium Wear For Modern Living</h1>
             <p className="t18 text-center"  >Discover our new range of soft clothes made for your daily look and your best days with the finest fabrics.</p>
-            <Button title="Shop Now" variant="btn1"></Button>
-            <Tag_Link title="Shop"/>
-            <Footer_Link title="Shop"/>
+           
         </section>
         </>
     );
