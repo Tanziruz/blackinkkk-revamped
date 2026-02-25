804592cfea93661188cadf3d5752b0dfeea8de46
Tanziruz <Tanziruz25@gmail.com>
2026-02-25 09:45:27 +0530

    Added a Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/components/Home/Hero";
+import ProductCatalog from "@/components/Home/ProductCatalog";
 
 export default function Home(){
   return (
     <>
+    {/* <Nav/> */}
     <Hero></Hero>
+    <ProductCatalog/>
     </>
   );
 }
---
+++ components/Home/ProductCatalog.tsx
@@ -0,0 +1,8 @@
+
+export default function ProductCatalog() {
+    return (
+        <>
+            <div className="h-dvh w-dvw"></div>
+        </>
+    );
+}
