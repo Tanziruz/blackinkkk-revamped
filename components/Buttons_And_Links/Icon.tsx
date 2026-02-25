63c77e6fdaea6c32fd6de25104831621d8370cb6
Tanziruz <Tanziruz25@gmail.com>
2026-02-25 10:46:13 +0530

    Add mobile nav bar support and nav bar animations.

--- components/Buttons_And_Links/Icon.tsx
+++ components/Buttons_And_Links/Icon.tsx
@@ -3,24 +3,27 @@ import type { LucideIcon } from "lucide-react";
 import { ArrowUpRight } from "lucide-react";
 
 interface SearchProps {
-    variant?: "variant1" | "variant2";
+    variant?: "variant1" | "variant2" | "variant3";
     icon: LucideIcon;
+    onClick?: () => void;
+    className?: string;
 }
 
-export default function Icon({ variant = "variant1", icon: Icon }: SearchProps) {
+export default function Icon({ variant = "variant1", icon: IconComponent, onClick, className }: SearchProps) {
     return (
-        <>
-            <div className={clsx(
-                "w-9.25 h-9 hover:cursor-pointer hover:scale-110 flex justify-center transition duration-200 items-center flex-wrap rounded-full ",
+        <div
+            onClick={onClick}
+            className={clsx(
+                "w-9.25 h-9 hover:cursor-pointer hover:scale-110 flex justify-center transition duration-200 items-center rounded-full",
                 {
                     "bg-white-15 hover:bg-white-30": variant === "variant1",
                     "bg-white hover:bg-white-80 shadow-md": variant === "variant2",
-                }
-            )}>
-                <div className="w-auto h-auto">
-                    <Icon color={variant === "variant1" ? "#ffffff" : "#000000"} />
-                </div>
-            </div>
-        </>
+                    "bg-black hover:bg-black/80": variant === "variant3",
+                },
+                className
+            )}
+        >
+            <IconComponent color={variant === "variant2" ? "#000000" : "#ffffff"} />
+        </div>
     );
 }
